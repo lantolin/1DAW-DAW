98c93c05e3c5b87e34656845dfce8d8fcc1d4df0
lantolin <luis.antolin@gmail.com>
2021-04-10 22:15:51 +0200

    correccion PHP

--- UT2/cgis/hola-mundo.php
+++ UT2/cgis/hola-mundo.php
@@ -1,8 +1,16 @@
 #!/usr/bin/php
-
 <?php
-  
-  echo 'Content-type: text/plain\n\n';
+ echo 'Content-type: text/plain
 
-  print 'Hola, Mundo soy un script de PHP.';
+Hola, Mundo soy un script de PHP.
+';  
+
+// importantes diferencias con la ejecucion con mod_php que veremos despues
+// 1. el fichero necesita cumplir con todo lo necesario para que el S.O lo ejecute
+//  1a. permisos
+//  1b. la línea #!/interprete
+// 2. tiene que cumplir con el estandar CGI y mandar el content-type
+
+// Cuando veamos mod_php esto ya no es necesario
 ?>
+
