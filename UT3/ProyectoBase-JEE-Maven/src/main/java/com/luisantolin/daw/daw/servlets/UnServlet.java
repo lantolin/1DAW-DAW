3092501e22dc58281500bdb9b56554b63f3cddf0
Luis Antolin <luis.antolin@gmail.com>
2022-05-06 14:23:15 +0200

    cambios estéticos en el Servlet para APPSERV.03

--- UT3/ProyectoBase-JEE-Maven/src/main/java/com/luisantolin/daw/daw/servlets/UnServlet.java
+++ UT3/ProyectoBase-JEE-Maven/src/main/java/com/luisantolin/daw/daw/servlets/UnServlet.java
@@ -1,38 +1,26 @@
 package com.luisantolin.daw.daw.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UnServlet
- */
 public class UnServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UnServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+   
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+    	PrintWriter salida = response.getWriter(); 
+    	String rutaDelContexto = request.getContextPath();
+		salida.append("Este bonito Servlet se encuentra en: ");
+		salida.append( rutaDelContexto );
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
