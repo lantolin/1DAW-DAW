32256a5903ebe4fa3993af03b941b4ab257cae4e
Luis Antolin <luis.antolin@gmail.com>
2024-05-09 10:17:55 +0200

    reorganizacion y mejora de cgis

--- uts/UT2/cgis2/hola-nombre.php
+++ uts/UT2/cgis2/hola-nombre.php
@@ -1,5 +1,22 @@
-<?php
- $nombre = $_GET['nombre'];
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nombre=$_GET['nombre'];
+    $server=$_SERVER['SERVER_SOFTWARE'];
+    $ip=$_SERVER['REMOTE_ADDR'];
+    ?>
 
- echo 'Hola ' . $nombre;
-?>
+  <h1>Hola mundo y hola persona</h1>
+  <p>A las buenas tardes, que sepas que estás usando el servidor <code>[<?php echo $server ?>]</code> y tu IP es: <code>[<?php echo $ip ?>]</code> </p>
+  <p>Si quieres que te salude enviame tu nombre en el parametro  <code>nombre</code> de la URL Query </p>
+  <p>Hola 
+<strong><?php echo $nombre ?></strong>, ¿como te va?
+</body>
+</html>
+
